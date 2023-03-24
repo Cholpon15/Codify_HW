fca5d6cde85d7e6550e6bf35400f22cb6c3c564c
Cholpon15 <kutmanbekova15@gmail.com>
2023-03-24 21:15:39 +0600

    add files

---
+++  hw l2/main.js
@@ -0,0 +1,31 @@
+//! Задание 1
+
+let admin = "Джон" ;
+let name = "Джон";
+
+console.log (admin)
+
+//! Задание 2
+let str = 'Привет Мир'
+console.log (str)
+
+let str1 = 'Привет '
+let str2 = 'Мир'
+
+ console.log (str1 + str2)
+
+
+//! Задание 3
+let num = 3
+console.log( num)
+
+let a = 10
+let b = 2
+console.log (a + b)
+
+console.log( a - b)
+console.log( a / b)
+console.log(a * b)
+
+
+
